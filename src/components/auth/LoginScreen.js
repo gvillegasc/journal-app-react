35f8fc9d90fbe28bcb09174caca55caef85cf374
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-08-15 13:10:18 -0500

    Añadiendo interfaz de login

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -3,7 +3,15 @@ import React from 'react';
 export const LoginScreen = () => {
 	return (
 		<div>
-			<h1>Login Screen</h1>
+			<h3>Login</h3>
+
+			<form>
+				<input type="text" placeholder="Email" name="email" />
+				<input type="password" placeholder="Passowrd" name="passowrd" />
+				<button type="submit">Login</button>
+				<hr></hr>
+				google
+			</form>
 		</div>
 	);
 };
