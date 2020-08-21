4a4a61b183f1fd38f35001bbd42acbc3d9135d31
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-08-20 19:44:13 -0500

    Creando componente NothingSelected

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Sidebar } from './Sidebar';
+import { NothingSelected } from './NothingSelected';
 
 export const JournalScreen = () => {
 	return (
@@ -7,7 +8,7 @@ export const JournalScreen = () => {
 			<Sidebar />
 
 			<main>
-				<h1>Main content</h1>
+				<NothingSelected />
 			</main>
 		</div>
 	);
