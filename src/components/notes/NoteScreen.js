70d8d5e6662c69bb71bda9f0b98aa47467a02efd
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-08-21 19:17:24 -0500

    Dando estilos al formulario de notas

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,27 @@ export const NoteScreen = () => {
 	return (
 		<div className="notes__main-content">
 			<NotesAppBar />
+
+			<div className="notes__content">
+				<input
+					type="text"
+					placeholder="Some awesome title"
+					className="notes__title-input"
+					autoComplete="off"
+				></input>
+
+				<textarea
+					placeholder="What happened todas"
+					className="notes__textarea"
+				></textarea>
+
+				<div className="notes__image">
+					<img
+						src="https://ichef.bbci.co.uk/news/410/cpsprodpb/113AC/production/_113427507_solar_orbiter_eui-fullsun01.jpg"
+						alt="imagen"
+					/>
+				</div>
+			</div>
 		</div>
 	);
 };
