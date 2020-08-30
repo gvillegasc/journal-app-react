dc0bcaac06d71e3c3d6fdb0d625d96207a722fcb
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-08-29 19:53:45 -0500

    Creando vista para validar si un usuario esta logueado o no

--- src/routes/AppRoute.js
+++ src/routes/AppRoute.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	BrowserRouter as Router,
 	Route,
@@ -14,14 +14,24 @@ import { login } from '../actions/auth';
 
 export const AppRoute = () => {
 	const dispatch = useDispatch();
+	const [checking, setChecking] = useState(true);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
 	useEffect(() => {
 		firebase.auth().onAuthStateChanged((user) => {
 			if (user?.uid) {
 				dispatch(login(user.uid, user.displayName));
+				setIsLoggedIn(true);
+			} else {
+				setIsLoggedIn(false);
 			}
+			setChecking(false);
 		});
-	}, [dispatch]);
+	}, [dispatch, setChecking, setIsLoggedIn]);
+
+	if (checking) {
+		return <h1>Espere...</h1>;
+	}
 
 	return (
 		<div>
