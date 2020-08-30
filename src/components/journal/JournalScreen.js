7418637786bd80d2e29f87c2a5ff861619e999ec
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-08-30 14:08:33 -0500

    Creando reducer de notas

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Sidebar } from './Sidebar';
 import { NoteScreen } from '../notes/NoteScreen';
-// import { NothingSelected } from './NothingSelected';
+import { useSelector } from 'react-redux';
+import { NothingSelected } from './NothingSelected';
 
 export const JournalScreen = () => {
+	const { active } = useSelector((state) => state.notes);
 	return (
 		<div className="journal__main-context">
 			<Sidebar />
 
-			<main>
-				{/* <NothingSelected /> */}
-				<NoteScreen />
-			</main>
+			<main>{active ? <NoteScreen /> : <NothingSelected />}</main>
 		</div>
 	);
 };
---
+++ src/reducers/notesReducer.js
@@ -0,0 +1,23 @@
+// {
+//     notes: [],
+//     active: [],
+//     active: {
+//         id: '12332dsafdansdna12',
+//         title: '',
+//         body: '',
+//         imageUrl: '',
+//         date: 2020-02-10
+//     }
+// }
+
+const initialState = {
+	notes: [],
+	active: null,
+};
+
+export const notesReducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
