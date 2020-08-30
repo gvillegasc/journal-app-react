7e555707f20c3d51b1bb5df5401521d3734bc60a
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-08-30 15:45:57 -0500

    Seleccionando nota cuando sea crea

--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -10,6 +10,8 @@
 //     }
 // }
 
+import { types } from '../types/types';
+
 const initialState = {
 	notes: [],
 	active: null,
@@ -17,6 +19,14 @@ const initialState = {
 
 export const notesReducer = (state = initialState, action) => {
 	switch (action.type) {
+		case types.notesActive:
+			return {
+				...state,
+				active: {
+					...action.payload,
+				},
+			};
+
 		default:
 			return state;
 	}
