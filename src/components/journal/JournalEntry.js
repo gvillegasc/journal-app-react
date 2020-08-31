c3d067d285d8df6cfcb7c8935b75a9ddb9ff1cbb
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-08-31 12:22:15 -0500

    Mostrando listado de notas en el sidebar

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,28 +1,28 @@
 import React from 'react';
+import moment from 'moment';
 
-export const JournalEntry = () => {
+export const JournalEntry = ({ id, date, title, body, url }) => {
+	const noteDate = moment(date);
 	return (
 		<div className="journal__entry pointer">
-			<div
-				className="journal__entry-picture"
-				style={{
-					backgroundSize: 'cover',
-					backgroundColor: 'blue',
-					backgroundImage:
-						'url(https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRMls1RWL101r8l9vOXkCt21N-sHTJajKd3Dg&usqp=CAU)',
-				}}
-			></div>
+			{url && (
+				<div
+					className="journal__entry-picture"
+					style={{
+						backgroundSize: 'cover',
+						backgroundColor: 'blue',
+						backgroundImage: `url(${url})`,
+					}}
+				></div>
+			)}
 			<div className="journal__entry-body">
-				<p className="journal__entry-title">Un nuevo dia</p>
-				<p className="journal__entry-content">
-					Eu minim irure sint enim reprehenderit magna nostrud anim est aliqua
-					do aliquip culpa.
-				</p>
+				<p className="journal__entry-title">{title}</p>
+				<p className="journal__entry-content">{body}</p>
 			</div>
 
 			<div className="journal__entry-date-box">
-				<span>Monday</span>
-				<h4>28</h4>
+				<span>{noteDate.format('dddd')}</span>
+				<h4>{noteDate.format('Do')}</h4>
 			</div>
 		</div>
 	);
