c6d1c795e93adb4d5f6de9ea7998cae52f7983cd
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-08-31 15:32:20 -0500

    Creando funcion para actualizar notas

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,6 +1,7 @@
 import { db } from '../firebase/firebase-config';
 import { types } from '../types/types';
 import { loadNotes } from '../helpers/loadNotes';
+import Swal from 'sweetalert2';
 
 export const startNewNote = () => {
 	return async (dispatch, getState) => {
@@ -47,5 +48,18 @@ export const startSaveNote = (note) => {
 		const noteToFirestore = { ...note };
 		delete noteToFirestore.id;
 		await db.doc(`${uid}/journal/notes/${note.id}`).update(noteToFirestore);
+		dispatch(refreshNote(note.id, noteToFirestore));
+		Swal.fire('Saved', note.title, 'success');
 	};
 };
+
+export const refreshNote = (id, note) => ({
+	type: types.notesUpdated,
+	payload: {
+		id,
+		note: {
+			id,
+			...note,
+		},
+	},
+});
